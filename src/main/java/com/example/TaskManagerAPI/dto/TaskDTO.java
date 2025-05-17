19c3f5a7448b3706266608bd9061bd76e12def48
Ahmbs <arthurhenriquemotabritosantos@gmail.com>
2025-05-16 21:38:46 -0300

    Consertando a data esperada pelo JSON

--- src/main/java/com/example/TaskManagerAPI/dto/TaskDTO.java
+++ src/main/java/com/example/TaskManagerAPI/dto/TaskDTO.java
@@ -1,6 +1,7 @@
 package com.example.TaskManagerAPI.dto;
 
 import com.example.TaskManagerAPI.model.Task.Priority;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,6 +12,9 @@ public record TaskDTO(
         @NotBlank @Size(max = 500) String description,
         @NotBlank @Size(max = 50) String assignee,
         @NotNull Priority priority,
-        @NotNull LocalDate deadline
+
+        @NotNull
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+        LocalDate deadline
 ) {
 }
